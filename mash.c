83a4374a28b550564e65e2097763b0a049575f81
Mathias Estrup <32955302+mestru17@users.noreply.github.com>
2026-06-29 13:18:38 +0200

    refactor: replace isspace usages
    The behavior of the `isspace` function depends on the system locale. Also,
    the function has an annoying gotcha where you have to always cast a
    `char` argument to `unsigned char` or risk undefined behavior because a
    signed `char` might get sign extended when passed as an `int` argument.
    
    We don't intend to support multiple locales. Also, we are working with
    `char`s. So let's just replace `isspace` with our own simple
    `is_whitespace` function that ignores locales and works on `char`
    instead of `int`.

--- mash.c
+++ mash.c
@@ -1,8 +1,12 @@
-#include <ctype.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+
+static int is_whitespace(char c) {
+    return c == ' ' || c == '\f' || c == '\n' || c == '\r' || c == '\t' ||
+           c == '\v';
+}
 
 int main() {
     for (;;) {
@@ -16,9 +20,10 @@ int main() {
 
         char *cmd;
         size_t cmdlen;
-        for (cmd = line; isspace(*cmd); cmd++)
+        for (cmd = line; is_whitespace(*cmd); cmd++)
             ;
-        for (cmdlen = 0; cmd[cmdlen] != '\0' && !isspace(cmd[cmdlen]); cmdlen++)
+        for (cmdlen = 0; cmd[cmdlen] != '\0' && !is_whitespace(cmd[cmdlen]);
+             cmdlen++)
             ;
 
         if (cmdlen == 4 && strncmp(cmd, "exit", cmdlen) == 0)
@@ -26,10 +31,10 @@ int main() {
 
         if (cmdlen == 4 && strncmp(cmd, "echo", cmdlen) == 0) {
             char *next;
-            for (next = cmd + cmdlen; isspace(*next); next++)
+            for (next = cmd + cmdlen; is_whitespace(*next); next++)
                 ;
             for (; *next != '\0' && *next != '\n'; next++)
-                if (!isspace(*(next - 1)) || !isspace(*next))
+                if (!is_whitespace(*(next - 1)) || !is_whitespace(*next))
                     putchar(*next);
             printf("\n");
         } else if (cmdlen == 3 && strncmp(cmd, "pwd", cmdlen) == 0) {
